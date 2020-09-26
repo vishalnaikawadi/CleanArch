49fee20c2929c50d87394578daf7f83448b8616f
vishal <vishal.naikawadi@leadschool.in>
2020-09-26 19:50:35 +0530

    comment added

--- app/src/main/java/com/bcg/life/view/activity/SplashActivity.kt
+++ app/src/main/java/com/bcg/life/view/activity/SplashActivity.kt
@@ -12,6 +12,8 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>() {
 
     lateinit var splashViewModel: SplashViewModel
 
+    //This is the only change
+
     override fun init() {
         splashViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
         splashViewModel.testMethod()
